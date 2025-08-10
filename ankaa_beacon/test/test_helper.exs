7067aff2b2bebb95775438c26324bd9fcdeb51b4
qweliant <dmtorcode@tutanota.com>
2025-08-10 18:46:43 -0400

    test should pass?

--- ankaa_beacon/test/test_helper.exs
+++ ankaa_beacon/test/test_helper.exs
@@ -5,7 +5,6 @@ Application.put_env(:ecto, :primary_key_type, :id)
 
 # Start Ecto repositories
 Ecto.Adapters.SQL.Sandbox.mode(Ankaa.Repo, :manual)
-Ecto.Adapters.SQL.Sandbox.mode(Ankaa.TimescaleRepo, :manual)
 
 # Start the application
 {:ok, _} = Application.ensure_all_started(:ex_machina)
