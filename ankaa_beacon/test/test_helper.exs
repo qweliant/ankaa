2fc8d0acc3e4ae40808d79e0fcd767e966486e2d
qweliant <dmtorcode@tutanota.com>
2025-08-23 11:20:56 -0400

    maybe this fixes failing test

--- ankaa_beacon/test/test_helper.exs
+++ ankaa_beacon/test/test_helper.exs
@@ -3,6 +3,8 @@ ExUnit.start()
 # Configure Ecto for support and tests
 Application.put_env(:ecto, :primary_key_type, :id)
 
+Application.ensure_all_started(:ankaa)
+
 # Start Ecto repositories
 Ecto.Adapters.SQL.Sandbox.mode(Ankaa.Repo, :manual)
 
