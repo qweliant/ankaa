31626b99709c969d805e7d5541e61dde8bb3b634
qweliant <dmtorcode@tutanota.com>
2025-08-23 13:08:29 -0400

    ummmmm. ok

--- ankaa_beacon/test/test_helper.exs
+++ ankaa_beacon/test/test_helper.exs
@@ -3,9 +3,6 @@ ExUnit.start()
 # Configure Ecto for support and tests
 Application.put_env(:ecto, :primary_key_type, :id)
 
-# Start the application and all its supervisors (including the Repo)
-Application.ensure_all_started(:ankaa)
-
 # Start Ecto repositories
 Ecto.Adapters.SQL.Sandbox.mode(Ankaa.Repo, :manual)
 
