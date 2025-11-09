f24d25ed1f716c915d504ebd23915db0fab91a66
qweliant <dmtorcode@tutanota.com>
2025-11-09 18:37:58 -0500

    fix messages to update notifications

--- ankaa_beacon/priv/repo/migrations/20250618013954_add_messages_table.exs
+++ ankaa_beacon/priv/repo/migrations/20250618013954_add_messages_table.exs
@@ -5,12 +5,13 @@ defmodule Ankaa.Repo.Migrations.AddMessagesTable do
     create table(:messages, primary_key: false) do
       add(:id, :binary_id, primary_key: true)
       add(:sender_id, references(:users, type: :binary_id, on_delete: :delete_all))
-      add(:patient_id, references(:users, type: :binary_id, on_delete: :delete_all))
+      add(:patient_id, references(:patients, type: :binary_id, on_delete: :delete_all))
       add(:content, :text)
       add(:read, :boolean, default: false)
       timestamps()
     end
 
     create(index(:messages, [:patient_id]))
+    create(index(:messages, [:sender_id]))
   end
 end
