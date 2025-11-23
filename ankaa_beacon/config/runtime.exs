77dd69b450ed61bb426c75e4507bce093a6bd70a
qweliant <dmtorcode@tutanota.com>
2025-11-23 18:37:42 -0500

    use https

--- ankaa_beacon/config/runtime.exs
+++ ankaa_beacon/config/runtime.exs
@@ -62,7 +62,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "safehemo.com"
+  host = System.get_env("PHX_HOST") || "https://safehemo.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :ankaa, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
